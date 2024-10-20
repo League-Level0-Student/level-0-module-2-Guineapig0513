ba3ee9316b39a8cb8e66b3256213606c7b6e4c95
league <league@imac-20.attlocal.net>
2024-10-19 17:52:43 -0700

    Guinea pig angy

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -17,7 +17,24 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if(randomNumber==(0)){
+			JOptionPane.showMessageDialog(null, "You are awesome!");
+		}
+		if(randomNumber==(1)){
+			JOptionPane.showMessageDialog(null, "You can be whatever you want, I mean you can be.... A AVOCADO!");
+		}
+		if(randomNumber==(2)){
+			JOptionPane.showMessageDialog(null, "You can be kind :)!");
+		}
+		if(randomNumber==(3)){
+			JOptionPane.showMessageDialog(null, "Remember not to be hard on yourself :D!");
+		}
+		if(randomNumber==(4)){
+			JOptionPane.showMessageDialog(null, "Wow look at yourself!... Amazing!");
+		}
+		if(randomNumber==(5)){
+			JOptionPane.showMessageDialog(null, "Work hard to achieve dreams! LIKE BE A AVOCADO!");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
