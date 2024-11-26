4874d48ec8df6b506dc449debfbefc68559d9386
league <league@imac-16.attlocal.net>
2024-11-25 18:30:47 -0800

    Hi floof is cute

---
+++ src/_03_string_conversion/_1_intro/Stringuhm.java
@@ -0,0 +1,11 @@
+package _03_string_conversion._1_intro;
+
+import javax.swing.JOptionPane;
+
+public class Stringuhm {
+	public static void main(String[] args) {
+		String quantity = JOptionPane.showInputDialog("Enter a number :D                      Why are you still reading just follow instructions >:(");
+		int num = Integer.parseInt(quantity);
+		JOptionPane.showInputDialog("I HATE THE NUMBER "+num+ "! WHY DID YOU CHOOSE THAT NUMBER???!!");
+	}
+}
